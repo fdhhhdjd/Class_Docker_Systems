1de026a03fe4d5e309bddea167817d1d15058e0f
fdhhhdjd <nguyentientai10@gmail.com>
2024-03-27 17:30:43 +0700

    feat: add redis

--- 4.redis-docker/script/script_redis.sh
+++ 4.redis-docker/script/script_redis.sh
@@ -1,5 +1,10 @@
 export TZ=Asia/Ho_Chi_Minh
 
+send_notification() {
+    # Log the timestamp of the backup
+    echo "$(date) - Log backup completed" >> /usr/local/bin/script/logs/log_script_redis_date.txt
+}
+
 while true; do
     # Date VN
     date=$(date +"%Y%m%d_%H%M%S")
@@ -7,6 +12,10 @@ while true; do
     # Backup Data
     echo "Backing up data..."
     redis-cli -h $REDIS_HOST -a $REDIS_PASSWORD -p $REDIS_PORT BGSAVE
+
+    # Logs time date backup
+    send_notification
+
 
     # release data redis show byte done
     echo "Released $(redis-cli -h $REDIS_HOST -a $REDIS_PASSWORD -p $REDIS_PORT  MEMORY RELEASE $MEMORY_RELEASE_MAX --statistics | grep -oE "[0-9]+") bytes of memory" --- DONE
@@ -17,6 +26,10 @@ while true; do
     # Restore Data from Dump File
     echo "Restoring data from dump file..."
     redis-cli -h $REDIS_HOST -a $REDIS_PASSWORD -p $REDIS_PORT -x set mykey < /data/dump.rdb
+
+
+    # logrotate -f /usr/local/bin/logs/logrotate.conf
+    logrotate /usr/local/bin/logs/logrotate.conf
     
     # Show Time last backup and date 
     echo $timelastbackup - $date    
--- 4.redis-docker/script/start_redis.sh
+++ 4.redis-docker/script/start_redis.sh
@@ -3,7 +3,10 @@
 redis-server /usr/local/etc/redis/redis.conf &
 
 # Run your script
-/usr/local/bin/script/script_redis.sh
+/usr/local/bin/script/script_redis.sh & 
+
+# Run Log
+logrotate /usr/local/bin/logs/logrotate.conf 
 
 # Keep the container running
 tail -f /dev/null
